2df2e7af7b79dc3e55eade0d38717e2c4e0ad2e1
basilk <basilkottilingal@gmail.com>
2025-03-09 06:36:11 +0100

    --

--- include/hedge-mesh.h
+++ include/hedge-mesh.h
@@ -15,7 +15,7 @@ extern "C" {
   #define HMESH_MAX_NVARS   64
   #define HMESH_MAX_VARNAME 31
   
-  /* '_HmeshAttribute' : a list of memory blocks.
+  /* '_HmeshArray' : a list of memory blocks.
   .. It can be used to store nodes, or attributes of nodes
   .. like scalars etc.
   */
@@ -39,26 +39,23 @@ extern "C" {
     */
     int n; 
 
-  } _HmeshAttribute;
-
-  /* Scalar is one type of attribute */
-  typedef _HmeshAttribute _HmeshScalar;
+  } _HmeshArray;
 
   /* To add a block */
   extern 
-  void * HmeshAttributeAdd(_HmeshAttribute *, _Flag);
+  void * HmeshArrayAdd(_HmeshArray *, _Flag);
 
   /* To delete a block */
   extern 
-  _Flag HmeshAttributeRemove(_HmeshAttribute *, _Flag);
+  _Flag HmeshArrayRemove(_HmeshArray *, _Flag);
 
-  /* Create an new HmeshAttribute obj */
-  extern _HmeshAttribute * 
-  HmeshAttribute(char *, size_t, Mempool *);
+  /* Create an new HmeshArray obj */
+  extern _HmeshArray * 
+  HmeshArray(char *, size_t, Mempool *);
  
-  /* Destroy a HmeshAttribute obj*/
+  /* Destroy a HmeshArray obj*/
   extern
-  _Flag _HmeshAttributeDestroy(_HmeshAttribute *);
+  _Flag _HmeshArrayDestroy(_HmeshArray *);
  
   /* _DataType : Precision used in scientific computation.
   .. 32 bits is Preferred data type precision for GPU vectors.
@@ -73,40 +70,52 @@ extern "C" {
   
   typedef uint16_t _Index;
 
-  /* '_HmeshNode' : a node in the Linked list . 
-  .. A data node may represent a point, or a halfedge, 
-  .. or an oriented face (mostly triangle);
-  .. size(_HmeshNode) = 8 Bytes
-  */ 
-  typedef struct _HmeshNode {
-    /* 'next' and 'prev' : for linked list.
-    .. The linked list is a 'doubly linked list' 
-    .. only which can let you delete any occuppied index.
+  struct _HmeshNode {
+    /* Linked list of nodes. 
+    .. Structure of Array (SoA) format 
     */
-    _Index next, prev;
+    _HmeshArray * prev, * next;
 
-    /* 'pid' : processor ID 
+#ifdef _HMESH_MPI
+    /* In case of MPI, 
+    .. pid : processor id,
+    .. gid : global id of node
     */
-    int pid;
+    _HmeshArray * pid, * gid;
+#endif
+  };
 
-  }_HmeshNode;
+  struct _HmeshMap {
+    /* Mapping of indices.. 
+    .. Ex: (a) Half-edge mapping H = (V0, V1) . 
+    ..     (b) Triangle mapping  T = (H0, H1, H2); 
+    */
+    _HmeshArray * _0, * _1, * _2, * _3;
+  
+    /* Twin (half-edge mesh). (iff d>0)*/
+    _HmeshArray * index;
+  };
+
+  struct _HmeshScalar {
+    /* Scalar list of type double (or float in some cases)*/
+    _HmeshArray ** s;
+
+    /* Number of scalars */
+    _Flag nscalars; 
+
+  };
 
   /* cells of mesh : vertices/edges/triangle/tetrahedrons 
   */
   typedef struct _HmeshCells {
     /* Node : vertex/edge/face/tetrahedron */
-    _HmeshAttribute * nodes;
+    struct _HmeshNode   nodes;
 
-    /* Integer mapping. 
-    .. Ex: mapping to vertices or twins etc.
-    */
-    _HmeshAttribute * maps[5];
+    /* Apply only for d>0. */
+    struct _HmeshMap    maps;
 
-    /* Scalar list of type double (or float in some cases)*/
-    _HmeshScalar ** s;
-
-    /* Number of scalars */
-    _Flag nscalars; 
+    /* List of scalars */
+    struct _HmeshScalar scalars;
 
     /* type of manifoldcell */
     _Flag type;
