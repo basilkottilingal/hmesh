33492fba7b50b6bcee0ac422e2df4c4d7e08396c
basil <basilkotttilingal@gmail.com>
2025-05-16 05:34:26 +0200

    compiler:  - modfd ast pool

--- syntax/ast/memory.c
+++ syntax/ast/memory.c
@@ -2,37 +2,47 @@
 #include <stdio.h>
 
 #include <memory.h>
+  
+/*
+.. @ _AstPool : Datatype that stores all root information 
+..    related to memory handling
+*/
+typedef struct {
+  void ** blocks, * head;
+  int nblocks; 
+} _AstPool;
+_AstPool _H_AST_POOL_ = {0};
  
-
 /*
 .. Create a new memory block
-.. fixme : try malloc() with smaller size, if 1 MB fails.
 */ 
-static void * ast_memory_block ( void ) {
-  _MemoryHandler * m = &_H_AST_POOL_;
+static void ast_memory_block ( void ) {
+  _AstPool * m = &_H_AST_POOL_;
 
   size_t size = _H_AST_BLOCK_SIZE_;
 
-  do {
+  while (size >= 4096) {
+
     void * mem = malloc(size);
     if (!mem) {
       size = size >> 1;
       continue;
     }
     
-    m->nblocks++; 
-    m->blocks = (void **) realloc(m->blocks, (m->nblocks)* sizeof(void *));
-    m->blocks[max-1] = mem; 
+    p->nblocks++; 
+    p->blocks = (void **) realloc(p->blocks, (p->nblocks)* sizeof(void *));
+    p->blocks[max-1] = mem;
+    p->head = mem + size;
 
-    return mem;
+    return;
     
-  } while (0);  /* replace with size > _H_AST_MINIMUM_BLOCK_SIZE*/
+  } 
 
-  fprintf(stderr, "ast_memory_block() : malloc() failed!");
+  fprintf(stderr, "ast_memory_block() : failed!");
   fflush(stderr);
   exit(EXIT_FAILURE);
 
-  return NULL;
+  return;
   
 }
 
@@ -40,59 +50,41 @@ static void * ast_memory_block ( void ) {
 .. Deallocates all blocks. Should be called at the end of the pgm
 */
 void ast_deallocate_all() {
-  _MemoryHandler * m = &_H_AST_POOL_;
-  void ** blocks = m->blocks; 
+  _AstPool * p = &_H_AST_POOL_;
+  void ** blocks = p->blocks; 
   if(!blocks)
     return;
 
-  _H_AST_PTR_ iblock = 1 + _H_AST_PTR_B_(m->head);
+  int iblock = 1 + p->nblocks;
   while (iblock--) 
     free(blocks[iblock])
   free(blocks);
 
-  m->blocks = NULL;
-  m->head   = 0; 
+  p->blocks  = p->head = NULL;
+  p->nblocks = 0; 
 }
 
 /*
 .. @ ast_allocate() : API to allocate memory of size 'size' .
-..    return type : _H_AST_PTR_ (alias uint32_t) 
 */
-_H_AST_PTR_ ast_allocate ( size_t size ) {
+void ast_allocate ( size_t size ) {
 
   if(size > 4096) {
     fprintf(stderr, "ast_allocate() : size too large");
     fflush(stderr);
     exit(EXIT_FAILURE);
   }
-  _MemoryHandler * m = &_H_AST_POOL_;
-  
-  _H_AST_PTR_ iblock = _H_AST_PTR_B_ (m->head),
-    head = _H_AST_PTR_N_ (m->head);
 
-  if() {
-    /* 
-    .. In case we run out of free pages, create a new block
-    .. and divide it into pages
-    */
-    char * page = ast_memory_block();
-    _H_AST_PTR_ iblock = m->max - 1;
-    _H_AST_PTR_ npages = _H_AST_BLOCK_SIZE_ / _H_AST_PAGE_SIZE_;
-    for(_H_AST_PTR_ ip=0; ip<npages; ++ip, page += _H_AST_PAGE_SIZE_) {
-      _FreePage * fpage = (_FreePage *) page;
-      fpage->ipage  = _H_AST_PTR_ENCODE_ (iblock, ip, 0);
-      fpage->safety = _H_AST_PTR_SAFETY;
-      fpage->next   = m->fhead;
-      m->fhead      = fpage;
-    }
-  }
- 
-  /* 
-  .. Pop a page from free head
-  */ 
-  _FreePage * fpage = m->fhead;
-  m->fhead = fpage->next;
-  m->fhead->safety = 0;
-  _Page * page = (_Page*) m->fhead;
-    m->fhead = next;
+  _AstPool * p = &_H_AST_POOL_;
+
+  if( !p->nblocks ) 
+    ast_memory_block();
+
+  if(p->head - p->blocks[p->nblocks - 1] < size) 
+    ast_memory_block();
+
+  p->head -= size;
+
+  return p->head;
+
 }
--- syntax/ast/memory.h
+++ syntax/ast/memory.h
@@ -1,11 +1,7 @@
 #ifndef _H_AST_POOL_
 #define _H_AST_POOL_
 
-  /* A memory node, _H_AST_PTR_, (kind of pointer) is an integer that stores
-  .. adequate information (BLOCK NUMBER <12 bits> + BIT LOCATION<20 bits>) to 
-  .. locate the actual memory address. A uint32_t is used over unsigned int
-  .. for the reason it's a 32 bit datatype irrespective of the architecture,
-  .. unlike unsigned int.
+  /* 
   .. 
   .. The memory pool is an arena allocator, where you cannot deallocate.
   .. Each allocated node survive till the end of the pgm.
@@ -13,36 +9,14 @@
   .. only at the end of the program.
   ..
   .. A block of size 1 MB (2^20) is allocated each time you run out of memory. 
-  .. Theoretically you can represent upto 4GB (2^12 Blocks x 2^20 Bytes) using
-  .. _H_AST_PTR_ which we hope is more than adequate for our parser.
   ..
   .. Thread Safety : NOT THREAD SAFE
   */
 
-  #define _H_AST_BLOCK_SIZE_ 1<<20    /* 1 MB     */
-  #define _H_AST_20_BITS_    1048575  /* 2^20 - 1 */
-
-  #ifndef _AST_LONG_PRECISION_
-    typedef uint32_t _H_AST_PTR_ ;
-  #else
-    typedef uint64_t _H_AST_PTR_ ;
+  #ifndef _H_AST_BLOCK_SIZE_
+    #define _H_AST_BLOCK_SIZE_ (1<<20)    /* 1 MB     */
   #endif
 
-  #define _H_AST_PTR_B_(_i_)            ( _i_ >> 20 )
-  #define _H_AST_PTR_N_(_i_)            ( _i_ & _H_AST_20_BITS_ )
-  #define _H_AST_PTR_ENCODE_(_b_,_n_)   (  (_b_<<20) | (_n_)  )
-
-  /*
-  .. @ _MemoryHandler : Datatype that stores all root information 
-  ..    related to memory handling
-  ..    @ blocks : array of memory blocks
-  ..    @ head  : head of the available memory stack.
-  */
-  typedef struct {
-    void ** blocks;
-    _H_AST_PTR_ head; 
-  } _MemoryHandler;
-  _MemoryHandler _H_AST_POOL_ = {0};
 
   /*
   .. Following are the api functions repsectively to
@@ -53,12 +27,5 @@
   */
   extern _H_AST_PTR_ ast_allocate(size);
   extern void ast_deallocate_all();
-
-  /*
-  .. Type Cast Memory Location to the specified type.
-  */
-  #define _H_AST_PTR_ADDRESS_(_i_, _type_)         \
-    ( (_type_) ( ((char *) _H_AST_POOL_.blocks[ _H_AST_PTR_B_(_i_) ] )  +  \
-          _H_AST_PTR_N_(_i_)  )  )
   
 #endif
